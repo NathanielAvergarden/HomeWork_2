e66c9b94f1683d4a18757ed4df8a590b8836aa39
Nathaniel <na26435@mail.ru>
2022-09-12 21:30:20 +0300

    Решил задачи, но есть ошибки

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿//Домашнее задание
+// Задача №10
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine());
+int[] arrey = new int[3];
+int i = 0;
+bool result = int i < arrey.Length - 1;
+if(result)
+{
+    int i = int i + 1;
+}
+Console.WriteLine(arrey[i]);
+
+// Задача №13
+Console.WriteLine("Введи число");
+int number = int.Parse(Console.ReadLine());
+int[] array = new int[];
+int i = 2;
+int Length;
+bool result = int i < array.Length;
+if(result)
+{
+    Console.WriteLine(arrey[i]);
+}
+else
+{
+    Console.WriteLine("Число не имеет третьей цифры");
+}
+Console.WriteLine(result);
+
+
+
+// Задача №15
+Console.WriteLine("Введи число от 1 до 7");
+int number = int.Parse(Console.ReadLine());
+int[] arrey = {1;2;3;4;5;6;7};
+bool result = int i < arrey.Length;
+for (int i=0; int i < arrey.Length; i++)
+{
+    Console.WriteLine("Рабочий день");
+}
+else
+{
+    Console.WriteLine("Выходной день");
+}
+Console.WriteLine(result);
